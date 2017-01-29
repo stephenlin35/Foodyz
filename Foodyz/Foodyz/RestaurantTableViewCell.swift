c9d059415b8546242e40292dda2fabe772b8a2ab
stephenlin35 <stephenlin35@users.noreply.github.com>
2017-01-29 18:49:02 -0500

    Add files to upload

---
+++ Foodyz/Foodyz/RestaurantTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  RestaurantTableViewCell.swift
+//  Foodyz
+//
+//  Created by Stephen Lin on 1/29/17.
+//  Copyright © 2017 Stephen Lin. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
