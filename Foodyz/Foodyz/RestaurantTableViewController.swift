c2e1b98e615ccf30b06d355aad6fa9fcc4d0d8a8
stephenlin35 <stephenlin35@users.noreply.github.com>
2017-01-31 10:35:02 -0500

    Add social sharing and delete buttons

--- Foodyz/Foodyz/RestaurantTableViewController.swift
+++ Foodyz/Foodyz/RestaurantTableViewController.swift
@@ -13,6 +13,12 @@ class RestaurantTableViewController: UITableViewController {
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate"]
     
     var restaurantImages = ["cafedeadend.jpg", "cafeloisl.jpg", "forkeerestaurant.jpg", "bourkestreetbakery.jpg", "haighschocolate.jpg", "grahamavenuemeats.jpg", "fiveleaves.jpg", "cafelore.jpg", "confessional.jpg", "barrafina.jpg", "donostia.jpg"]
+    
+    var restaurantLocations = ["New York", "Paris", "Florida", "New York", "Louisiana", "New York", "San Francisco", "London", "Hong Kong", "Las Vegas"]
+    
+    var restaurantTypes = ["Coffee/Bakery", "Italian", "Western", "Coffee/Bakery", "Seafood", "Asian", "French", "Bakery", "Dessert"]
+    
+    var restaurantIsVisited = [Bool](count: 10, repeatedValue: false)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,10 +56,42 @@ class RestaurantTableViewController: UITableViewController {
         // Configure the cell...
         cell.nameLabel.text = restaurantNames[indexPath.row]
         cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row])
+        cell.locationLabel.text = restaurantLocations[indexPath.row]
+        cell.typeLabel.text = restaurantTypes[indexPath.row]
+        
+        if restaurantIsVisited[indexPath.row] {
+            cell.accessoryType = .Checkmark
+        } else {
+            cell.accessoryType = .None
+        }
         
         return cell
     }
-
+    
+    override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) ->[UITableViewRowAction]? {
+            // Social Sharing Button
+            let shareAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Share", handler: { (action, indexPath) -> Void in
+                let defaultText = "Just checking in at " + self.restaurantNames[indexPath.row]
+            if let imageToShare = UIImage(named: self.restaurantImages[indexPath.row]) {
+                let activityController = UIActivityViewController(activityItems: [defaultText, imageToShare], applicationActivities: nil)
+                self.presentViewController(activityController, animated: true, completion: nil)
+            }
+            })
+            
+            // Delete button
+            let deleteAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Delete", handler:  { (action, indexPath) -> Void in
+        
+                // Delete the row from the data source
+                self.restaurantNames.removeAtIndex(indexPath.row)
+                self.restaurantLocations.removeAtIndex(indexPath.row)
+                self.restaurantTypes.removeAtIndex(indexPath.row)
+                self.restaurantIsVisited.removeAtIndex(indexPath.row)
+                self.restaurantImages.removeAtIndex(indexPath.row)
+                self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            })
+            return [deleteAction, shareAction]
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
@@ -67,12 +105,54 @@ class RestaurantTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
+            restaurantNames.removeAtIndex(indexPath.row)
+            restaurantLocations.removeAtIndex(indexPath.row)
+            restaurantTypes.removeAtIndex(indexPath.row)
+            restaurantIsVisited.removeAtIndex(indexPath.row)
+            restaurantImages.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
+        }
+        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        /*
+        else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        } */
     }
     */
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // Create an option menu as an action sheet
+        let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .ActionSheet)
+        
+        // Add actions to the menu
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        optionMenu.addAction(cancelAction)
+        
+        let callActionHandler = { (action:UIAlertAction!) -> Void in
+            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .Alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(alertMessage, animated: true, completion: nil)
+        }
+        
+        let callAction = UIAlertAction(title: "Call " + "123-000-\(indexPath.row)", style: UIAlertActionStyle.Default, handler: callActionHandler)
+        optionMenu.addAction(callAction)
+        
+        let isVisitedAction = UIAlertAction(title: "I've been here", style: .Default,
+                handler: {
+                    (action:UIAlertAction!) -> Void in
+                
+                    let cell = tableView.cellForRowAtIndexPath(indexPath)
+                    cell?.accessoryType = .Checkmark
+                    self.restaurantIsVisited[indexPath.row] = true
+                })
+        optionMenu.addAction(isVisitedAction)
+        
+        // Display the menu
+        self.presentViewController(optionMenu, animated: true, completion: nil)
+    }
+    
+    
+    
 
     /*
     // Override to support rearranging the table view.
